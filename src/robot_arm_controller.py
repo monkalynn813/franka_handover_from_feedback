0cb446360f30e1ed7de30776e66b42ca25dfae5d
Jingyan Ling <jingyanling2018@u.northwestern.edu>
2020-08-25 15:41:29 +0800

    update impedance structure

---
+++ src/robot_arm_controller.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+import rospy
+import moveit_commander
+from franka_interface import ArmInterface, GripperInterface
+from franka_moveit import PandaMoveGroupInterface 
+from math import pi, sqrt
+import numpy as np
+import tf.transformations as tr
+import tf_conversions.posemath as pm
+from trajectory_generator import msg_to_se3, se3_to_msg
+
+#import rosmsg needed:
+from geometry_msgs.msg import Point, Quaternion, Pose, PoseStamped
+import moveit_msgs.msg
+from moveit_commander.conversions import pose_to_list
+from std_msgs.msg import String, Bool
+
+
+
+class Impedance_control():
+    def __init__(self,ptx=150,pty=150,ptz=150,pr=10,mode='static'):
+        #set stiffness:
+        self.P = np.zeros([6,6])
+        np.fill_diagonal(self.P,[ptx,pty,ptz,pr,pr,pr])
+        #set damping: critical damp
+        self.D= np.zeros([6,6])
+        np.fill_diagonal(self.D,2*np.sqrt(np.diag(self.P)))
+        #TODO: set inertia
+        
+
+        self.mode=mode
+
+        self.joint_names=['panda_joint1','panda_joint2','panda_joint3','panda_joint4','panda_joint5','panda_joint6','panda_joint7']
+        self.limb = ArmInterface()
+
+        if mode =='static':
+            self.panda_moveit_wrap = PandaMoveGroupInterface()
+            self.arm_group=self.panda_moveit_wrap._arm_group
+        #publisher:
+        # self.publisher_name = rospy.Publisher('topic_name',String)
+
+        #subscriber:
+        # self.subscriber_name = rospy.Subscriber('topic_name',String, self.subscriber_callback)
+
+        rate=rospy.Rate(10) 
+    
+    def get_reference_pose(self,target_pose=None):
+        #target_pose= geometry_msgs/Pose
+        if self.mode=='static':
+            self.go_static_test_pose()
+            rospy.sleep(2.0)
+            rospy.loginfo('---Moved to static test pose----')
+            self.ref_position=self.limb.endpoint_pose()['position']
+            self.ref_orientation=self.limb.endpoint_pose()['orientation']
+        elif self.mode=='dynamic':
+            self.ref_position=target_pose.position
+            self.ref_orientation=target_pose.orientation
+
+        
+    def go_static_test_pose(self):
+        rospy.loginfo("--moving to static test pose---")
+
+        T_standby=np.array([[-0.1815108 , -0.98090692,  0.06982443, -0.15],
+                            [ 0.00259123,  0.07052656,  0.99750654,  0.5],
+                            [-0.98338554,  0.18123914, -0.01025958,  0.5],
+                            [ 0.        ,  0.        ,  0.        ,  1. ]])
+
+        target_pose= se3_to_msg(T_standby)
+        self.arm_group.set_pose_target(target_pose)
+        self.arm_group.go(wait=True) 
+        self.arm_group.stop()
+        self.arm_group.clear_pose_targets()
+    
+    def control_loop(self,ref_position,ref_orientation):
+        #TODO: get coriolis
+        #get Jacobian
+        J = self.limb.zero_jacobian()
+        self.cur_poistion=self.limb.endpoint_pose()['position']
+        self.cur_orientation=self.limb.endpoint_pose()['orientation']
+
+        #compute deviation
+        
+
+
+    def publish_function(self,arg):
+        
+        data=1
+        
+        self.publisher_name.publish(data)
+
+    def subscriber_callback(self,data):
+
+        data+=1
+
+def main():
+    rospy.init_node("robot_arm_controller")
+    try:
+        project=ProjectName()  #if program run from init, otherwise put starting function
+        
+    except rospy.ROSInterruptException:pass
+        
+
+    rospy.spin()
+if __name__ == '__main__':
+	main()
+    
